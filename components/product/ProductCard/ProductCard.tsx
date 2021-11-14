3eae74f6e5dbf5dcf7299396996346afa109ca73
Mahdi Rezayi <mahdi.rezzai93@gmail.com>
2021-11-14 12:22:46 +0330

    15 Product Card - Home Page CSS

--- components/product/ProductCard/ProductCard.tsx
+++ components/product/ProductCard/ProductCard.tsx
@@ -3,6 +3,7 @@ import Link from 'next/link'
 import Image from 'next/image'
 
 import { Product } from '@common/types/product'
+import s from './ProductCard.module.css'
 
 interface Props {
   product: Product
@@ -13,15 +14,19 @@ const placeholderImage = '/product-image-placeholder.svg'
 const ProductCard: FC<Props> = ({ product }) => {
   return (
     <Link href={`/products/${product.slug}`}>
-      <a>
-        <div>
-          <h3>
+      <a className={s.root}>
+        <div className={s.productBg}></div>
+        <div className={s.productTag}>
+          <h3 className={s.productTitle}>
             <span>{product.name}</span>
           </h3>
-          <span>14 $</span>
+          <span className={s.productPrice}>
+            {product.price.value} {product.price.currencyCode}
+          </span>
         </div>
         {product.images && (
           <Image
+            className={s.productImage}
             src={product.images[0].url ?? placeholderImage}
             alt={product.name ?? 'Product image'}
             width={540}
